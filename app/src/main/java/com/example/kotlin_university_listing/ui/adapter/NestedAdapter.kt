3e20948e2180570023617aebc6441af8c11defd7
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-04-11 02:07:05 +0300

    Edit NestedAdapter

--- app/src/main/java/com/example/kotlin_university_listing/ui/adapter/NestedAdapter.kt
+++ app/src/main/java/com/example/kotlin_university_listing/ui/adapter/NestedAdapter.kt
@@ -3,6 +3,9 @@ package com.example.kotlin_university_listing.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin_university_listing.R
@@ -10,6 +13,10 @@ import com.example.kotlin_university_listing.R
 class NestedAdapter(private val mList:List<String>) : RecyclerView.Adapter<NestedAdapter.NestedAdapterHolder>(){
     inner class NestedAdapterHolder(view: View):RecyclerView.ViewHolder(view) {
         val mTv: TextView = itemView.findViewById(R.id.nestedItemText)
+        val linearLayout: LinearLayout = itemView.findViewById(R.id.nested_linear_layout)
+        val expandableLayout: RelativeLayout = itemView.findViewById(R.id.nested_expandable_layout)
+        val mArrowImage: ImageView = itemView.findViewById(R.id.nested_arro_imageview)
+        val nestedRecyclerView: RecyclerView = itemView.findViewById(R.id.nested_child_rv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NestedAdapter.NestedAdapterHolder {
