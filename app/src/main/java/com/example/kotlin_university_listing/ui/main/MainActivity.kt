6f84e0e97de9786a74e1bca6d2a322e4ffb0b036
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-04-21 16:47:47 +0300

    Added SecondLevelItem

--- app/src/main/java/com/example/kotlin_university_listing/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_university_listing/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_university_listing.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -14,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin_university_listing.R
 import com.example.kotlin_university_listing.data.model.Province
 import com.example.kotlin_university_listing.data.model.ProvinceResponse
+import com.example.kotlin_university_listing.data.model.University
 import com.example.kotlin_university_listing.data.remote.ServiceBuilder
 import com.example.kotlin_university_listing.ui.adapter.ThreeLevelListAdapter
 import retrofit2.Call
@@ -25,20 +27,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var expandableListView: ExpandableListView
 
     val cityNames = mutableListOf<String>()
-    private val q1 = arrayOf("List View", "Grid View")
-    private val q2 = arrayOf("Linear Layout", "Relative Layout")
-    private val q3 = arrayOf("Recycle View")
-    private val des1 = arrayOf("A layout that organizes its children into a single horizontal or vertical row. It creates a scrollbar if the length of the window exceeds the length of the screen.")
-    private val des2 = arrayOf("Enables you to specify the location of child objects relative to each other (child A to the left of child B) or to the parent (aligned to the top of the parent).")
-    private val des3 = arrayOf("This list contains linear layout information")
-    private val des4 = arrayOf("This list contains relative layout information,Displays a scrolling grid of columns and rows")
-    private val des5 = arrayOf("Under the RecyclerView model, several different components work together to display your data. Some of these components can be used in their unmodified form; for example, your app is likely to use the RecyclerView class directly. In other cases, we provide an abstract class, and your app is expected to extend it; for example, every app that uses RecyclerView needs to define its own view holder, which it does by extending the abstract RecyclerView.ViewHolder class.")
+    val universityNames = ArrayList<Array<String>>()
 
-    private val thirdLevelq1 = LinkedHashMap<String, Array<String>>()
-    private val thirdLevelq2 = LinkedHashMap<String, Array<String>>()
-    private val thirdLevelq3 = LinkedHashMap<String, Array<String>>()
+  //  private val des1 = arrayOf("A layout that organizes its children into a single horizontal or vertical row. It creates a scrollbar if the length of the window exceeds the length of the screen.")
+    //private val des2 = arrayOf("Enables you to specify the location of child objects relative to each other (child A to the left of child B) or to the parent (aligned to the top of the parent).")
+    //private val des3 = arrayOf("This list contains linear layout information")
+    //private val des4 = arrayOf("This list contains relative layout information,Displays a scrolling grid of columns and rows")
+    //private val des5 = arrayOf("Under the RecyclerView model, several different components work together to display your data. Some of these components can be used in their unmodified form; for example, your app is likely to use the RecyclerView class directly. In other cases, we provide an abstract class, and your app is expected to extend it; for example, every app that uses RecyclerView needs to define its own view holder, which it does by extending the abstract RecyclerView.ViewHolder class.")
 
-    private val secondLevel = ArrayList<Array<String>>()
+    //private val thirdLevelq1 = LinkedHashMap<String, Array<String>>()
+
+
+    //private val secondLevel = ArrayList<Array<String>>()
     private val data = ArrayList<LinkedHashMap<String, Array<String>>>()
 
 
@@ -59,10 +59,11 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun FavoriteListButton(){
-        val favoriteButton = findViewById<ImageView>(R.id.favorite_list)
+        val favoriteListButton = findViewById<ImageView>(R.id.favorite_list)
 
-        favoriteButton.setOnClickListener {
-
+        favoriteListButton.setOnClickListener {
+            val intent = Intent(this, FavoriteListActivity::class.java)
+            startActivity(intent)
         }
     }
     private fun RequestApi(){
@@ -78,8 +79,17 @@ class MainActivity : AppCompatActivity() {
                         for (i in 0..provinces.size-1 ) {
                             Log.e("University", provinces[i].province)
                             cityNames.add(provinces[i].province)
+                            val universitiesInProvince = provinces[i].universities.map { it.name }.toTypedArray()
+                            universityNames.add(universitiesInProvince)
+                            val thirdLevel = LinkedHashMap<String, Array<String>>()
+                            for (university in provinces[i].universities) {
+                                val desArray = university.address
+                                thirdLevel[university.name] = arrayOf(desArray)
+                            }
+                            data.add(thirdLevel)
+
                         }
-                        val threeLevelListAdapterAdapter = ThreeLevelListAdapter(applicationContext, cityNames.toTypedArray(), secondLevel, data)
+                        val threeLevelListAdapterAdapter = ThreeLevelListAdapter(applicationContext, cityNames.toTypedArray(), universityNames, data)
                         setUpAdapter(threeLevelListAdapterAdapter)
                         threeLevelListAdapterAdapter.notifyDataSetChanged() // Adaptördeki değişiklikleri güncelle
 
@@ -98,18 +108,18 @@ class MainActivity : AppCompatActivity() {
         /////////// Api çekmenin bittiği yer
     }
     private fun setUpAdapter(threeLevelListAdapterAdapter:ThreeLevelListAdapter) {
-        secondLevel.add(q1)
-        secondLevel.add(q2)
-        secondLevel.add(q3)
-        thirdLevelq1[q1[0]] = arrayOf(des1[0], des2[0])
-        thirdLevelq1[q1[1]] = des2
-        thirdLevelq2[q2[0]] = des3
-        thirdLevelq2[q2[1]] = des4
-        thirdLevelq3[q3[0]] = des5
+       // secondLevel.add(q1)
+        //secondLevel.add(q2)
+        //secondLevel.add(q3)
+        //thirdLevelq1[q1[0]] = arrayOf(des1[0], des2[0])
+        //thirdLevelq1[q1[1]] = des2
+       // thirdLevelq2[q2[0]] = des3
+       // thirdLevelq2[q2[1]] = des4
+       // thirdLevelq3[q3[0]] = des5
 
-        data.add(thirdLevelq1)
-        data.add(thirdLevelq2)
-        data.add(thirdLevelq3)
+        //data.add(thirdLevelq1)
+        //data.add(thirdLevelq2)
+        //data.add(thirdLevelq3)
 
         expandableListView = findViewById(R.id.expandable_listview)
         expandableListView.setAdapter(threeLevelListAdapterAdapter)
