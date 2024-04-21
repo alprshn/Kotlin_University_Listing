22ff84b215b8fbfcbc81449e2d3dbe9f6c44a8db
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-04-21 15:38:24 +0300

    Added Asset

--- app/src/main/java/com/example/kotlin_university_listing/ui/adapter/SecondLevelAdapter.kt
+++ app/src/main/java/com/example/kotlin_university_listing/ui/adapter/SecondLevelAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.kotlin_university_listing.R
 
 class SecondLevelAdapter(private val context: Context, private val headers: Array<String>, private val data: List<Array<String>>) : BaseExpandableListAdapter()  {
@@ -45,6 +47,27 @@ class SecondLevelAdapter(private val context: Context, private val headers: Arra
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = p2 ?: inflater.inflate(R.layout.row_second, null)
         val text = view.findViewById<TextView>(R.id.rowSecondText)
+        val favoriteButton = view.findViewById<ImageView>(R.id.favorite_button)
+        var checkFavorite: Boolean = false
+
+        favoriteButton.setOnClickListener {
+            val currentSrc = favoriteButton.drawable
+
+            // Mevcut src'yi kontrol ederek favori butonunun durumunu değiştirin
+            if (currentSrc == ContextCompat.getDrawable(context, R.drawable.empty_hearth)) {
+                // Eğer boş kalp görünüyorsa dolu kalp olarak değiştirin
+                favoriteButton.setImageResource(R.drawable.filled_hearth)
+            } else {
+                // Eğer dolu kalp görünüyorsa boş kalp olarak değiştirin
+                favoriteButton.setImageResource(R.drawable.empty_hearth)
+            }
+            // Burada favori butonuna tıklandığında yapılacak işlemi tanımlayabilirsiniz
+            // Örneğin, bir bildirim gösterebilir, favorilere ekleyebilir veya çıkarabilirsiniz
+            // İlgili özelliklere erişip işlem yapabilirsiniz.
+            // Örneğin, p0 ve p1 değerlerini kullanarak verilere erişebilirsiniz.
+        }
+
+
         val groupText = getGroup(p0).toString()
         text.text = groupText
         return view
